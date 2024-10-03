a1b2d0e3a64b53312f68afd4016442f5bff7bde4
gustur0 <alvesrochagadini@gmail.com>
2024-10-03 19:11:13 -0300

    Adicionando Entidades

--- src/main/java/com/github/gadini/checkpoint1/model/Produto.java
+++ src/main/java/com/github/gadini/checkpoint1/model/Produto.java
@@ -1,5 +1,47 @@
 package com.github.gadini.checkpoint1.model;
 
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Produto {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	@Column(nullable = false)
+	private String nome;
+	
+	@Column(nullable = false)
+	private BigDecimal valor;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public BigDecimal getValor() {
+		return valor;
+	}
+
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
 }
