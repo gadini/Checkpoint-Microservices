6527e2c4390246e65576ae5880fe2a765c32b065
gustur0 <alvesrochagadini@gmail.com>
2024-10-03 18:57:39 -0300

    Initial

---
+++ src/main/java/com/github/gadini/checkpoint1/model/Produto.java
@@ -0,0 +1,5 @@
+package com.github.gadini.checkpoint1.model;
+
+public class Produto {
+
+}
---
+++ src/test/java/com/github/gadini/checkpoint1/Checkpoint1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.gadini.checkpoint1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Checkpoint1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
